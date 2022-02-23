b151a7009c8745a22316548b44d8924a56955074
Maria <pashkovichma@gmail.com>
2022-02-23 11:02:29 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -38,8 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let res = '';
+    const keysArr = Object.keys(MORSE_TABLE);
+    let keysNew = [];
+    const valuesArr = Object.values(MORSE_TABLE);
+
+    for (let i = 0; i < keysArr.length; i++) {
+        let a = '';
+        for (let j = 0; j < keysArr[i].length; j++) {
+            if (keysArr[i][j] == '.') {
+            a = a + '10';
+            } else a = a + '11';
+        }
+        while (a.length < 10) {
+            a = '0' + a;
+        }
+        keysNew[i] = a;
+    }
+
+    for (let i = 0; i < expr.length; i = i + 10) {
+      for (let j = 0; j < keysNew.length; j++) {
+        if (expr.slice(i, i + 10) == keysNew[j]) {
+          res = res + valuesArr[j];
+          console.log('+');
+        }
+      }
+      if (expr.slice(i, i +10) == '**********') {
+        res = res + ' ';
+      }
+    }
+    return (res);
+  }
 
 module.exports = {
     decode
